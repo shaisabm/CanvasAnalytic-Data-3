eb9d73a3a9dbc6db8cc412a9a474b3a3cf0e6158
Shaisab Mistry <shaisabmistry@gmail.com>
2025-02-09 15:08:58 -0500

    set up sample data

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class SampleData(models.Model):
+    feature1 = models.CharField(max_length=100, default='sampleData')
+    feature2 = models.CharField(max_length=100, default='sampleData')
+    feature3 = models.CharField(max_length=100, default='sampleData')
+    feature4 = models.CharField(max_length=100, default='sampleData')
+    feature5 = models.CharField(max_length=100, default='sampleData')
+    feature6 = models.CharField(max_length=100, default='sampleData')
+    feature7 = models.CharField(max_length=100, default='sampleData')
+    feature8 = models.CharField(max_length=100, default='sampleData')
+    feature9 = models.CharField(max_length=100, default='sampleData')
+    feature10 = models.CharField(max_length=100, default='sampleData')
+
+
