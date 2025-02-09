3ad7483966041861cf2feccf399e2d672b885f72
Shaisab Mistry <shaisabmistry@gmail.com>
2025-02-09 15:08:44 -0500

    set up class based views for creating user and get data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer, SampleDataSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import SampleData
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+class GetSampleData(generics.ListAPIView):
+    serializer_class = SampleDataSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        return SampleData.objects.all()
+
+
+
+
+
+
